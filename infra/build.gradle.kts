2e5744aef7070551244e38a1f864bc605b79e6fb
선빈 <ehtjsqls@gmail.com>
2026-01-23 05:06:20 +0900

    feat: Infra 모듈 Gradle 빌드 설정 파일 추가
    - Kotlin 및 Spring Boot 플러그인을 포함한 Gradle 빌드 설정 파일을 추가했습니다.
    - 필요한 의존성을 정의하고, 테스트 플랫폼으로 JUnit을 설정했습니다.

---
+++ infra/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+group = "com.zzan"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":common"))
+    implementation(project(":user"))
+    implementation(project(":feed"))
+    implementation(project(":liquor"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("io.awspring.cloud:spring-cloud-aws-starter-s3:3.3.0")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
