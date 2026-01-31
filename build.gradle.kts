9bbb1848a0883d67ab711c85c0dde51ca47d5557
선빈 <ehtjsqls@gmail.com>
2026-02-01 04:55:25 +0900

    feat: build.gradle.kts에 allopen 플러그인 추가
    - Kotlin의 allopen 플러그인을 추가하여 특정 어노테이션을 가진 클래스를 자동으로 open으로 설정합니다.
    - jakarta.persistence.MappedSuperclass 어노테이션을 사용하는 클래스에 대해 allopen 플러그인을 구성했습니다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "1.9.25" apply false
     kotlin("plugin.spring") version "1.9.25" apply false
     kotlin("plugin.jpa") version "1.9.25" apply false
+    kotlin("plugin.allopen") version "1.9.25" apply false
     id("org.springframework.boot") version "3.5.4" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
 }
@@ -20,6 +21,10 @@ subprojects {
     apply(plugin = "org.jetbrains.kotlin.plugin.spring")
     apply(plugin = "io.spring.dependency-management")
     apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+
+    configure<org.jetbrains.kotlin.allopen.gradle.AllOpenExtension> {
+        annotation("jakarta.persistence.MappedSuperclass")
+    }
 
     dependencies {
         "testRuntimeOnly"("org.junit.platform:junit-platform-launcher")
@@ -40,4 +45,5 @@ subprojects {
     tasks.named<Test>("test") {
         useJUnitPlatform()
     }
+
 }
